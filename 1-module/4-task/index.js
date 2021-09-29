905093bf3859733d5fb0543ebc1acd78979eb3cd
OVSlepov <O.Slepov@hyundai-autoever.ru>
2021-09-29 15:24:30 +0300

    solved task#4 and #5

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
-  // ваш код...
+    let checkStr = '1XbeT now'.toLowerCase();
+    let checkStr2 = 'free xxxxx'.toLowerCase();
+    
+    return str.toLowerCase().includes(checkStr)||
+      str.toLowerCase().includes(checkStr2);
+  
 }
