f5fb12df3df6c153a8b40f0e28f2680f560c5ee1
OVSlepov <O.Slepov@hyundai-autoever.ru>
2021-10-18 13:26:27 +0300

    solved task #2 from lesson 3

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,9 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let result = [];
+  arr.forEach((value, index, array) => {
+    if (index==a||index==b||value==a||value==b){
+      result.push(value);
+    } 
+  });
+  return result;
 }
